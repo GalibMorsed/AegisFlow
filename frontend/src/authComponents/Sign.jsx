e81fb3c80d0552fb09fb2fd73a74aa0b43c8bad8
Saurabh <sauravthakulla683@gmail.com>
2025-10-13 00:33:30 +0530

    Database connected to atlas

--- frontend/src/authComponents/Sign.jsx
+++ frontend/src/authComponents/Sign.jsx
@@ -1,20 +1,58 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function SignIn() {
+export default function Login() {
   return (
-    <div>
-      <h2>Sign In</h2>
-      <form>
-        <label>
-          Email:
-          <input type="email" name="email" required />
-        </label>
-        <label>
-          Password:
-          <input type="password" name="password" required />
-        </label>
-        <button type="submit">Sign In</button>
-      </form>
+    <div className="login-container">
+      <div
+        className="login-image"
+        style={{
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&w=1000&q=80')",
+        }}
+      ></div>
+
+      <div className="login-form-section">
+        <div className="login-box">
+          <h2 className="login-title">Sign In</h2>
+
+          <form className="login-form">
+
+            <div className="form-group">
+              <label>Name</label>
+              <input type="text" name="name" required placeholder="Enter your Username" />
+            </div>
+
+            <div className="form-group">
+              <label>Email</label>
+              <input type="email" name="email" required placeholder="Enter your email" />
+            </div>
+
+            <div className="form-group">
+              <label>Password</label>
+              <input type="password" name="password" required placeholder="Enter your password" />
+            </div>
+
+            <button type="submit" className="login-btn">
+              Login
+            </button>
+          </form>
+
+          <p className="login-text">
+           Already have an account?{" "}
+            <Link to="/" className="link">
+              Signup
+            </Link>
+          </p>
+
+          <p className="login-text">
+            Forgot your password?{" "}
+            <Link to="/reset-password" className="link">
+              Reset
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
