3daff2b931aae52f1e127844c1c03d60d4773a64
Galib Morsed <morsedgalib982@gmail.com>
2025-10-14 00:03:39 +0530

    Backend configuration successfully completed and connected with MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,55 +1,27 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv");
-dotenv.config();
-const mongoose = require("mongoose");  // <- ADD THIS
-const usermodel = require("./model/model");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
+const authRoutes = require("./routes/authRoutes");
 
+require("dotenv").config();
+require("./Models/db.js");
+const PORT = process.env.PORT || 8000;
 
-mongoose.connect(process.env.URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected ✅"))
-.catch((err) => console.log("MongoDB connection error ❌", err));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
+app.get("/hii", (req, res) => {
+  res.send("Hello World");
 });
 
-app.post("/signup", async (req, res) => {
-    const { name, age, email, password } = req.body;
-    let user = await usermodel.findOne({ email: email });
-    if (user) {
-        return res.status(400).json({ message: "User already exists" });
-    }
-    let users =  await usermodel.create({ name, age, email, password});
-    let token = jwt.sign({ id: users._id }, "secretkey");
-    res.cookie("token", token, { httpOnly: true });
-    res.status(201).json({ message: "User created successfully", users });
-})
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/auth", authRoutes);
 
-app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    let user = await usermodel.findOne({ email: email });
-    if (!user) {
-        return res.status(400).json({ message: "User not found" });
-    }
-    if (user.password !== password) {
-        return res.status(400).json({ message: "Invalid credentials" });
-    }
-    let token = jwt.sign({ id: user._id }, "secretkey");
-    res.cookie("token", token, { httpOnly: true });
-    res.status(200).json({ message: "Login successful", user });
-})
-
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+app.listen(PORT, (error) => {
+  if (error) {
+    console.error("Error starting server:", error);
+  } else {
     console.log(`Server is running on ${PORT}`);
+  }
 });
